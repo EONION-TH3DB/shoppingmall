6c457f840e92395f8439ba023c3ffeae08e5eb23
EONION-TH3DB <eonion888@gmail.com>
2023-07-21 00:08:37 +0900

    sho

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='text-lg p-4'>
+      <sticky-header class="header-wrapper header-wrapper--border-bottom">
+        <header id="theme-header" class="header header--top-center page-width header-width header--has-menu">
+          <div></div>
+          <h1></h1>
+          <div></div>
+        </header>
+      </sticky-header>
+    </div>
+  )
+}
